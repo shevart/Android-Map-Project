2db746cc6a6933ca193dc522f78483a21e52bb48
Artem Shevchenko <shevartsoft@gmail.com>
2017-10-10 18:37:35 +0300

    add useful utils classes

---
+++ app/src/main/java/com/shevart/google_map/util/LogUtil.java
@@ -0,0 +1,25 @@
+package com.shevart.google_map.util;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+@SuppressWarnings("unused")
+public class LogUtil {
+    private static final String TAG = "<- Map-Project-Tag ->";
+
+    public static void e(@NonNull String e) {
+        Log.e(TAG, e);
+    }
+
+    public static void empty() {
+        Log.e(TAG, "");
+    }
+
+    public static void e(@NonNull Throwable e) {
+        e.printStackTrace();
+    }
+
+    public static void d(@NonNull String e) {
+        Log.d(TAG, e);
+    }
+}
---
+++ app/src/main/java/com/shevart/google_map/util/UiNotificationsUtils.java
@@ -0,0 +1,27 @@
+package com.shevart.google_map.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+@SuppressWarnings("WeakerAccess")
+public class UiNotificationsUtils {
+    private static boolean debug = true;
+
+    public static void showEmptyToast(@NonNull Context context, @NonNull String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showDevMessage(@NonNull Context context, @NonNull String msg) {
+        if (debug) {
+            showEmptyToast(context, msg);
+            LogUtil.e(msg);
+        }
+    }
+
+    public static class Extra {
+        public static void developerSeeToLogsMsg(@NonNull Context context) {
+            showDevMessage(context, "Developer, go to logs!");
+        }
+    }
+}
